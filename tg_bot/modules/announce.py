19cead6e44f18f635d9710d80691725f8cc5a744
Luke <me@luuke.ml>
2021-12-10 17:22:57 +0200

    announce: remove useless perm check

--- tg_bot/modules/announce.py
+++ tg_bot/modules/announce.py
@@ -1,18 +1,14 @@
-
-from tg_bot.modules.helper_funcs.chat_status import u_can_change_info
+import html
 
 from telegram import Update
-
 from telegram.ext import CallbackContext
+from telegram.utils.helpers import mention_html
 
 from tg_bot import spamcheck
 from tg_bot.modules.log_channel import loggable
-
-import tg_bot.modules.sql.logger_sql as sql
 from tg_bot.modules.helper_funcs.decorators import kigcmd
 
-import html
-from telegram.utils.helpers import mention_html
+import tg_bot.modules.sql.logger_sql as sql
 
 from ..modules.helper_funcs.anonymous import user_admin as u_admin, AdminPerms, resolve_user as res_user, UserClass
 
@@ -27,34 +23,30 @@ def announcestat(update: Update, context: CallbackContext) -> str:
         message = update.effective_message
         chat = update.effective_chat
         user = res_user(u, message.message_id, chat)
-        if u_can_change_info(chat, user):
-            if args[0].lower() in ["on", "yes", "true"]:
-                sql.enable_chat_log(update.effective_chat.id)
-                update.effective_message.reply_text(
-                    "I've enabled announcemets in this group. Now any admin actions in your group will be announced."
-                )
-                logmsg = (
-                    f"<b>{html.escape(chat.title)}:</b>\n"
-                    f"#ANNOUNCE_TOGGLED\n"
-                    f"Admin actions announcement has been <b>enabled</b>\n"
-                    f"<b>Admin:</b> {mention_html(user.id, user.first_name)}\n"
-                )
-                return logmsg
-            elif args[0].lower() in ["off", "no", "false"]:
-                sql.disable_chat_log(update.effective_chat.id)
-                update.effective_message.reply_text(
-                    "I've disabled announcemets in this group. Now admin actions in your group will not be announced."
-                )
-                logmsg = (
-                    f"<b>{html.escape(chat.title)}:</b>\n"
-                    f"#ANNOUNCE_TOGGLED\n"
-                    f"Admin actions announcement has been <b>disabled</b>\n"
-                    f"<b>Admin:</b> {mention_html(user.id, user.first_name)}\n"
-                )
-                return logmsg
-        else:
-            message.reply_text("You lack the {} right!".format('can_change_info'.upper()))
-            return ''
+        if args[0].lower() in ["on", "yes", "true"]:
+            sql.enable_chat_log(update.effective_chat.id)
+            update.effective_message.reply_text(
+                "I've enabled announcemets in this group. Now any admin actions in your group will be announced."
+            )
+            logmsg = (
+                f"<b>{html.escape(chat.title)}:</b>\n"
+                f"#ANNOUNCE_TOGGLED\n"
+                f"Admin actions announcement has been <b>enabled</b>\n"
+                f"<b>Admin:</b> {mention_html(user.id, user.first_name)}\n"
+            )
+            return logmsg
+        elif args[0].lower() in ["off", "no", "false"]:
+            sql.disable_chat_log(update.effective_chat.id)
+            update.effective_message.reply_text(
+                "I've disabled announcemets in this group. Now admin actions in your group will not be announced."
+            )
+            logmsg = (
+                f"<b>{html.escape(chat.title)}:</b>\n"
+                f"#ANNOUNCE_TOGGLED\n"
+                f"Admin actions announcement has been <b>disabled</b>\n"
+                f"<b>Admin:</b> {mention_html(user.id, user.first_name)}\n"
+            )
+            return logmsg
     else:
         update.effective_message.reply_text(
             "Give me some arguments to choose a setting! on/off, yes/no!\n\n"
