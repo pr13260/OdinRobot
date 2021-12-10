bb47f22cb1350b783f8ba0f942af1927155b2342
Luke <me@luuke.ml>
2021-12-10 17:12:55 +0200

    extract admin actions using ChatMemberUpdated
    log changes to Log Channel
    and optionally send to chat itself

--- tg_bot/modules/sql/logger_sql.py
+++ tg_bot/modules/sql/logger_sql.py
@@ -4,9 +4,6 @@ from sqlalchemy import Column, String, Boolean
 
 from tg_bot.modules.sql import BASE, SESSION
 
-
-
-
 class LoggerSettings(BASE):
     __tablename__ = "chat_log_settings"
     chat_id = Column(String(14), primary_key=True)
@@ -19,25 +16,21 @@ class LoggerSettings(BASE):
     def __repr__(self):
         return "<Chat log setting {} ({})>".format(self.chat_id, self.setting)
 
-
 LoggerSettings.__table__.create(checkfirst=True)
 
 LOG_SETTING_LOCK = threading.RLock()
 LOGSTAT_LIST = set()
-
 
 def enable_chat_log(chat_id):
     with LOG_SETTING_LOCK:
         chat = SESSION.query(LoggerSettings).get(str(chat_id))
         if not chat:
             chat = LoggerSettings(chat_id, True)
-
         chat.setting = True
         SESSION.add(chat)
         SESSION.commit()
         if str(chat_id) in LOGSTAT_LIST:
             LOGSTAT_LIST.remove(str(chat_id))
-
 
 def disable_chat_log(chat_id):
     with LOG_SETTING_LOCK:
@@ -50,11 +43,8 @@ def disable_chat_log(chat_id):
         SESSION.commit()
         LOGSTAT_LIST.add(str(chat_id))
 
-
 def does_chat_log(chat_id):
     return str(chat_id) not in LOGSTAT_LIST
-
-
 
 def __load_chat_log_stat_list():
     global LOGSTAT_LIST
@@ -64,7 +54,6 @@ def __load_chat_log_stat_list():
         }
     finally:
         SESSION.close()
-
 
 def migrate_chat(old_chat_id, new_chat_id):
     with LOG_SETTING_LOCK:
