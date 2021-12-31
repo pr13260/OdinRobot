230431c8034aaa3fd195c9023f1238931e85fa07
blank X <blankie@nixnetmail.com>
2022-01-01 01:45:25 +0530

    New helper function to upload text to privatebin
    Signed-off-by: Sayan Biswas <sayan@intellivoid.net>

--- tg_bot/modules/helper_funcs/misc.py
+++ tg_bot/modules/helper_funcs/misc.py
@@ -1,4 +1,5 @@
 from typing import Dict, List
+import typing
 from uuid import uuid4
 from asyncio import sleep
 from telegram.message import Message
@@ -8,6 +9,14 @@ from telegram.error import TelegramError
 from tg_bot.modules.helper_funcs.string_handling import button_markdown_parser_v2, reply_button_parser_v2
 from tg_bot.modules.helper_funcs.msg_types import Types
 
+import requests
+import json
+import zlib
+import base64
+from urllib.parse import urlparse, urljoin, urlunparse
+import base58
+from Crypto import Random, Hash, Protocol
+from Crypto.Cipher import AES
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
     def __eq__(self, other):
@@ -33,9 +42,8 @@ def split_message(msg: str) -> List[str]:
         else:
             result.append(small_msg)
             small_msg = line
-    else:
-        # Else statement at the end of the for loop, so append the leftover string.
-        result.append(small_msg)
+    # Else statement at the end of the for loop, so append the leftover string.
+    result.append(small_msg)
 
     return result
 
@@ -124,14 +132,12 @@ def build_keyboard(buttons):
 
 
 def revert_buttons(buttons):
-    res = ""
-    for btn in buttons:
-        if btn.same_line:
-            res += "\n[{}](buttonurl://{}:same)".format(btn.name, btn.url)
-        else:
-            res += "\n[{}](buttonurl://{})".format(btn.name, btn.url)
-
-    return res
+    return "".join(
+        "\n[{}](buttonurl://{}:same)".format(btn.name, btn.url)
+        if btn.same_line
+        else "\n[{}](buttonurl://{})".format(btn.name, btn.url)
+        for btn in buttons
+    )
 
 
 def build_keyboard_parser(bot, chat_id, buttons):
@@ -243,3 +249,18 @@ def build_keyboard_alternate(buttons):
             keyb.append([InlineKeyboardButton(btn[0], url=btn[1])])
 
     return keyb
+def upload_text(data: str) -> typing.Optional[str]:
+    passphrase = Random.get_random_bytes(32)
+    salt = Random.get_random_bytes(8)
+    key = Protocol.KDF.PBKDF2(passphrase, salt, 32, 100000, hmac_hash_module=Hash.SHA256)
+    compress = zlib.compressobj(wbits=-15)
+    paste_blob = compress.compress(json.dumps({'paste': data}, separators=(',', ':')).encode()) + compress.flush()
+    cipher = AES.new(key, AES.MODE_GCM)
+    paste_meta = [[base64.b64encode(cipher.nonce).decode(), base64.b64encode(salt).decode(), 100000, 256, 128, 'aes', 'gcm', 'zlib'], 'syntaxhighlighting', 0, 0]
+    cipher.update(json.dumps(paste_meta, separators=(',', ':')).encode())
+    ct, tag = cipher.encrypt_and_digest(paste_blob)
+    resp = requests.post('https://bin.nixnet.services', headers={'X-Requested-With': 'JSONHttpRequest'}, data=json.dumps({'v': 2, 'adata': paste_meta, 'ct': base64.b64encode(ct + tag).decode(), 'meta': {'expire': '1week'}}, separators=(',', ':')))
+    data = resp.json()
+    url = list(urlparse(urljoin('https://bin.nixnet.services', data['url'])))
+    url[5] = base58.b58encode(passphrase).decode()
+    return urlunparse(url)
